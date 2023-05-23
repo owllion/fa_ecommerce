9db67eec8a2742efcd1590155b0809b5f5d6188f
owllion <defrag55345@gmail.com>
2023-05-23 18:26:09 +0800

    update db url

--- app/database/db.py
+++ app/database/db.py
@@ -6,10 +6,14 @@ from sqlalchemy.orm import sessionmaker
 db_name = config("DB_NAME")
 db_user = config("DB_USER")
 db_password = config("DB_PASSWORD")
-db_host = config("DB_HOST")
+# db_host = config("DB_HOST")
 db_port = config("DB_PORT")
+unix_socket_path = config("UNIX_SOCKET_PATH")
+cloud_sql_instance_name = config("CLOUD_SQL_INSTANCE_NAME")
 
-db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+# db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db_url = f"mysql+pymysql://{db_user}:{db_password}@/{db_name}?unix_socket={unix_socket_path}/{cloud_sql_instance_name}"
 
 engine = create_engine(db_url)
 
